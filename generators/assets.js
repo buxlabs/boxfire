30fb19b4ff8ac0a4ffdeb2f2090464d1d6768c1b
Emil Ajdyna <emil@ajdyna.com>
2023-08-28 23:39:19 +0200

    Ignore directories

--- generators/assets.js
+++ generators/assets.js
@@ -3,7 +3,7 @@ const { dirname } = require("path")
 const { glob } = require("glob")
 
 module.exports = async function generateAssets({ input, output }) {
-  const files = await glob(`${input}/assets/**/*`)
+  const files = await glob(`${input}/assets/**/*`, { nodir: true, dot: false })
   for (const file of files) {
     const out = file.replace(input, output)
     const dir = dirname(out)
