ee3a16c15bb9c4ec6dbf4677b6c3c89fbad4c711
Emil Ajdyna <emil@ajdyna.com>
2023-08-29 20:10:04 +0200

    Add a warning for files without extensions

--- generators/assets.js
+++ generators/assets.js
@@ -1,12 +1,19 @@
 const { copyFile, mkdir } = require("fs/promises")
-const { dirname } = require("path")
+const { basename, dirname } = require("path")
 const { glob } = require("glob")
+
+const IMAGE_EXTENSIONS = ["jpg", "jpeg", "png", "gif"]
 
 module.exports = async function generateAssets({ input, output }) {
   const files = await glob(`${input}/assets/**/*`, { nodir: true, dot: false })
   const assets = []
   for (const file of files) {
     const out = file.replace(input, output)
+    const filename = basename(out)
+    const [name, extension] = filename.split(".")
+    if (IMAGE_EXTENSIONS.includes(extension) && !name.match(/\d+x\d+/)) {
+      console.error(`Please rename ${file} and add the size`)
+    }
     const dir = dirname(out)
     await mkdir(dir, { recursive: true })
     await copyFile(file, out)
