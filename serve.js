737a9ee73e8b3e309b389a0835d17f974f90da31
Emil Ajdyna <emil@ajdyna.com>
2023-12-10 22:49:00 +0100

    Add compression

--- serve.js
+++ serve.js
@@ -1,7 +1,9 @@
 const express = require("express")
+const compression = require("compression")
 
 module.exports = async function serve({ static, port }) {
   const app = express()
+  app.use(compression())
   app.use(express.static(static))
   app.listen(port)
   return app
