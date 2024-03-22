725549a4582b8cccda31dd7af20e1a3b30c3f385
Emil Ajdyna <emil@ajdyna.com>
2024-03-22 10:02:59 +0100

    Windows updates for better path handling

--- generators/pages.js
+++ generators/pages.js
@@ -1,10 +1,10 @@
 const { writeFile, mkdir } = require("fs/promises")
-const { dirname, join, relative } = require("path")
+const { dirname, join, relative, sep } = require("path")
 const { glob } = require("glob")
 const { compile } = require("boxwood")
 
 async function findViews(input) {
-  const paths = await glob(`${input}/views/**/*.js`)
+  const paths = await glob(`${input}/views/**/*.js`, { absolute: true })
   return paths.filter((file) => !file.endsWith(".test.js"))
 }
 
@@ -21,12 +21,12 @@ function getCanonical({ domain, currentPath }) {
 }
 
 function isPartial(view) {
-  const parts = dirname(view).split("/")
+  const parts = dirname(view).split(sep)
   return parts[parts.length - 1].startsWith("_")
 }
 
 function isDynamicView(view) {
-  const parts = view.split("/")
+  const parts = view.split(sep)
   const filename = parts[parts.length - 1]
   return filename.startsWith("[") && filename.endsWith("].js")
 }
@@ -47,7 +47,7 @@ async function generatePage({
     currentPath,
   })
   const html = template({ currentPath, canonical, paths, ...data })
-  const out = path.replace(`${input}/views`, output).replace(".js", ".html")
+  const out = path.replace(join(input, "views"), output).replace(".js", ".html")
   const dir = dirname(out)
   await mkdir(dir, { recursive: true })
   await writeFile(out, html, "utf8")
@@ -66,10 +66,10 @@ module.exports = async function generatePages({ input, output, domain }) {
     }
     if (isDynamicView(view)) {
       const path = dirname(view)
-      const parts = path.split("/")
+      const parts = path.split(sep)
       const last = parts[parts.length - 1]
       const dir = join(input, "assets/content", last)
-      const files = await glob(dir + "/**/*.js")
+      const files = await glob(dir + "/**/*.js", { absolute: true })
       for (const file of files) {
         const fn = require(file)
         const data = await fn()
